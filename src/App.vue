5af8308d9670c096ee774a486b448d29ae55c787
BogSala <zero959512@gmail.com>
2026-02-20 23:04:11 +0200

    Basic

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup>
 import packageJson from '../package.json'
 import HpCalculator from './components/HpCalculator.vue'
+import PointBuy from './components/PointBuy.vue'
 
 console.log('Current version is ' + packageJson.version)
 </script>
 
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-header mb-4">
     <div class="container">
       <a class="navbar-brand" href="#">D&D Tools</a>
     </div>
@@ -16,10 +17,14 @@ console.log('Current version is ' + packageJson.version)
     <div class="row">
       <div class="col-lg-12">
         <HpCalculator />
+        <PointBuy />
       </div>
     </div>
   </main>
 </template>
 
 <style lang="scss">
+.bg-header {
+  background-color: #787b7e !important;
+}
 </style>
---
+++ src/components/PointBuy.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { reactive, computed } from 'vue'
+
+const abilities = reactive([
+  { key: 'STR', name: 'Strength', value: 8 },
+  { key: 'DEX', name: 'Dexterity', value: 8 },
+  { key: 'CON', name: 'Constitution', value: 8 },
+  { key: 'INT', name: 'Intelligence', value: 8 },
+  { key: 'WIS', name: 'Wisdom', value: 8 },
+  { key: 'CHA', name: 'Charisma', value: 8 }
+])
+
+const totalPoints = 27
+
+const spentPoints = computed(() => 0)
+const remainingPoints = computed(() => totalPoints)
+
+function increase(ability) {
+  console.log('increase', ability.key)
+}
+
+function decrease(ability) {
+  console.log('decrease', ability.key)
+}
+
+function resetAll() {
+  console.log('reset')
+}
+</script>
+
+<template>
+  <section class="card shadow-sm mb-4 hp-calculator">
+    <div class="card-header bg-secondary text-white">
+      <h2 class="h5 mb-0">HP Calculator</h2>
+    </div>
+    <div class="card-body">
+    </div>
+  </section>
+</template>\
+
+<style scoped></style>
